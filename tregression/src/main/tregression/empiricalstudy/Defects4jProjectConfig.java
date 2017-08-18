dc6b043c5039bee5c2b25f33e5b1648f9200aedb
Yun Lin <llmhyy@gmail.com>
2017-08-18 22:39:59 +0800

    deal with the change of source file folder of Defects4J Lang project

--- tregression/src/main/tregression/empiricalstudy/Defects4jProjectConfig.java
+++ tregression/src/main/tregression/empiricalstudy/Defects4jProjectConfig.java
@@ -18,24 +18,29 @@ public class Defects4jProjectConfig {
 		this.buildFolder = buildFolder;
 	}
 	
-	public static Defects4jProjectConfig getD4JConfig(String projectName) {
+	public static Defects4jProjectConfig getD4JConfig(String projectName, int bugID) {
 		if(projectName.equals("Chart")) {
 			return new Defects4jProjectConfig("tests", "source", "build-tests", "build", "build");
 		}
 		else if (projectName.equals("Closure")) {
-			return new Defects4jProjectConfig("test", "src", "build/classes", "build/test", "build");
+			return new Defects4jProjectConfig("test", "src", "build/test", "build/classes", "build");
 		}
 		else if (projectName.equals("Lang")) {
-			return new Defects4jProjectConfig("src/test/java", "src/main/java", "target/classes", "target/tests", "target");
+			if(bugID<42){
+				return new Defects4jProjectConfig("src/test/java", "src/main/java", "target/tests", "target/classes", "target");				
+			}
+			else{
+				return new Defects4jProjectConfig("src/test", "src/java", "target/tests", "target/classes", "target");
+			}
 		}
 		else if (projectName.equals("Math")) {
-			return new Defects4jProjectConfig("src/test/java", "src/main/java", "target/classes", "target/test-classes", "target");
+			return new Defects4jProjectConfig("src/test/java", "src/main/java", "target/test-classes", "target/classes", "target");
 		}
 		else if (projectName.equals("Mockito")) {
-			return new Defects4jProjectConfig("test", "src", "build/classes/main", "build/classes/test", "build");
+			return new Defects4jProjectConfig("test", "src", "build/classes/test", "build/classes/main", "build");
 		}
 		else if (projectName.equals("Time")) {
-			return new Defects4jProjectConfig("src/test/java", "src/main/java", "target/classes", "target/test-classes", "target");
+			return new Defects4jProjectConfig("src/test/java", "src/main/java", "target/test-classes", "target/classes", "target");
 		}
 		
 		return null;
