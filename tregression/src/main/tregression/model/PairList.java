60f631d3c13c5de4b4607c78af61d513532152d6
Yun Lin <llmhyy@gmail.com>
2017-10-12 16:10:23 +0800

    optimize the trace matching by creating more hashmaps

--- tregression/src/main/tregression/model/PairList.java
+++ tregression/src/main/tregression/model/PairList.java
@@ -1,16 +1,32 @@
 package tregression.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import microbat.model.trace.TraceNode;
 
 public class PairList {
 	private List<TraceNodePair> pairList = new ArrayList<>();
+	private Map<Integer, TraceNodePair> beforeNodeToPairMap = new HashMap<>();
+	private Map<Integer, TraceNodePair> afterNodeToPairMap = new HashMap<>();
 
 	public PairList(List<TraceNodePair> pairList) {
 		super();
 		this.pairList = pairList;
+		for(TraceNodePair pair: pairList){
+			TraceNode beforeNode = pair.getBeforeNode();
+			TraceNode afterNode = pair.getAfterNode();
+			
+			if(beforeNode!=null){
+				beforeNodeToPairMap.put(beforeNode.getOrder(), pair);
+			}
+			
+			if(afterNode!=null){
+				afterNodeToPairMap.put(afterNode.getOrder(), pair);
+			}
+		}
 	}
 
 	public List<TraceNodePair> getPairList() {
@@ -26,21 +42,29 @@ public class PairList {
 	}
 
 	public TraceNodePair findByAfterNode(TraceNode node) {
-		for(TraceNodePair pair: pairList){
-			if(pair.getAfterNode().equals(node)){
-				return pair;
-			}
+//		for(TraceNodePair pair: pairList){
+//			if(pair.getAfterNode().equals(node)){
+//				return pair;
+//			}
+//		}
+		if(node==null){
+			return null;
 		}
-		return null;
+		TraceNodePair pair = afterNodeToPairMap.get(node.getOrder());
+		return pair;
 	}
 	
 	public TraceNodePair findByBeforeNode(TraceNode node) {
-		for(TraceNodePair pair: pairList){
-			if(pair.getBeforeNode().equals(node)){
-				return pair;
-			}
+//		for(TraceNodePair pair: pairList){
+//			if(pair.getBeforeNode().equals(node)){
+//				return pair;
+//			}
+//		}
+		if(node==null){
+			return null;
 		}
-		return null;
+		TraceNodePair pair = beforeNodeToPairMap.get(node.getOrder());
+		return pair;
 	}
 	
 	public int size(){
