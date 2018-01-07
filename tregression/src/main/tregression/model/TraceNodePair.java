e82234573e35c8864a1d9e8ab121bb5515762842
Yun Lin <llmhyy@gmail.com>
2018-01-07 17:31:41 +0800

    refactoring

--- tregression/src/main/tregression/model/TraceNodePair.java
+++ tregression/src/main/tregression/model/TraceNodePair.java
@@ -233,7 +233,7 @@ public class TraceNodePair {
 									if(!mutatedSubVarValue.equals(mutatedVar)){
 										String varID = mutatedSubVarValue.getVarID();
 										if(!varID.contains(":") && !varID.contains(VirtualVar.VIRTUAL_PREFIX)){
-											String order = mutatedTrace.findDefiningNodeOrder(Variable.READ, afterdNode, false,
+											String order = mutatedTrace.findDefiningNodeOrder(Variable.READ, afterdNode, 
 													varID, mutatedSubVarValue.getAliasVarID());
 											varID = varID + ":" + order;
 										}
