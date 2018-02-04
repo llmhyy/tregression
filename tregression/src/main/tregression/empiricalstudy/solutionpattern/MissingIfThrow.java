8f112a22580e75e7e222c87032b870c023e6f049
Yun Lin <llmhyy@gmail.com>
2018-02-04 17:30:40 +0800

    clean

--- tregression/src/main/tregression/empiricalstudy/solutionpattern/MissingIfThrow.java
+++ tregression/src/main/tregression/empiricalstudy/solutionpattern/MissingIfThrow.java
@@ -1,8 +1,6 @@
 package tregression.empiricalstudy.solutionpattern;
 
-import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.ThrowStatement;
 
