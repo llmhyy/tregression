271cbb3a3641645d02c9dfb3bce3050eb4428bc4
Yun Lin <llmhyy@gmail.com>
2018-04-27 00:06:33 +0800

    minor

--- tregression/src/main/tregression/handler/RetrieveAllDefect4jRegressionsHandler.java
+++ tregression/src/main/tregression/handler/RetrieveAllDefect4jRegressionsHandler.java
@@ -3,12 +3,19 @@
  */
 package tregression.handler;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+
+import tregression.empiricalstudy.ReadEmpiricalTrial;
+import tregression.empiricalstudy.TrialReader;
 
 /**
  * @author LLT
@@ -40,6 +47,14 @@ public class RetrieveAllDefect4jRegressionsHandler extends RegressionRetrieveHan
 		
 		String[] projects = {"Chart", "Closure", "Lang", "Math", "Mockito", "Time"};
 		int[] bugNum = {26, 133, 65, 106, 38, 27};
+		
+		String fileName = "defects4j0.old.xlsx";
+		Map<ReadEmpiricalTrial, ReadEmpiricalTrial> map = new HashMap<>();
+		try {
+			map = new TrialReader().readXLSX(fileName);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	
 		int count = 0;
 		for(int i=0; i<projects.length; i++) {
@@ -48,7 +63,36 @@ public class RetrieveAllDefect4jRegressionsHandler extends RegressionRetrieveHan
 				if(count <= skippedNum || count > endNum) {
 					continue;
 				}
+				
+				
+				
+				if(!map.isEmpty()){
+					ReadEmpiricalTrial tmp = new ReadEmpiricalTrial();
+					tmp.setProject(projects[i]);
+					tmp.setBugID(String.valueOf(j));
+					
+					ReadEmpiricalTrial t = map.get(tmp);
+					if(t==null){
+						System.err.println(projects[i]+"-"+j+" is missing.");
+						continue;
+					}
+					
+					String deadEndType = t.getDeadEndType();
+					if(deadEndType==null || 
+							!(deadEndType.equals("control") || deadEndType.equals("data"))){
+						continue;
+					}
+					
+//					String exception = t.getException();
+//					if(exception==null || !exception.contains("over long")){
+//						continue;
+//					}
+				}
+						
 				try {
+					
+					System.out.println("***working on the " + j + "th bug of " + projects[i] + " project.");
+					
 					retrieveRegression(projects[i], String.valueOf(j));
 				} catch (Exception e) {
 					e.printStackTrace();
