74946489d3f0bb048fc9c6791f55f7d0f3bbaa18
lylytran <lylypp6987@gmail.com>
2018-05-07 17:57:57 +0800

    generate regression-test report when running all defects4j

--- tregression/src/main/tregression/handler/AllDefects4jHandler.java
+++ tregression/src/main/tregression/handler/AllDefects4jHandler.java
@@ -2,6 +2,7 @@ package tregression.handler;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +16,8 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 
+import experiment.utils.report.ExperimentRegressionTest;
+import experiment.utils.report.TextComparationRule;
 import microbat.Activator;
 import tregression.empiricalstudy.DeadEndCSVWriter;
 import tregression.empiricalstudy.DeadEndRecord;
@@ -40,8 +43,8 @@ public class AllDefects4jHandler extends AbstractHandler {
 				int skippedNum = 0;
 				int endNum = 500;
 				
-				String[] projects = {"Chart", "Closure", "Lang", "Math", "Mockito", "Time"};
-				int[] bugNum = {26, 133, 65, 106, 38, 27};
+//				String[] projects = {"Chart", "Closure", "Lang", "Math", "Mockito", "Time"};
+//				int[] bugNum = {26, 133, 65, 106, 38, 27};
 				
 				String fileName = "defects4j0.old.xlsx";
 				Map<ReadEmpiricalTrial, ReadEmpiricalTrial> map = new HashMap<>();
@@ -57,95 +60,106 @@ public class AllDefects4jHandler extends AbstractHandler {
 //				String[] projects = {"Lang"};
 //				int[] bugNum = {65};
 				
-//				String[] projects = {"Time"};
-//				int[] bugNum = {27};
+				String[] projects = {"Time"};
+				int[] bugNum = {2};
 				
 				String prefix = Activator.getDefault().getPreferenceStore().getString(TregressionPreference.REPO_PATH) + File.separator;
 				
 				int count = 0;
-				for(int i=0; i<projects.length; i++) {
-					
-					for(int j=1; j<=bugNum[i]; j++) {
+				File defects4jFile = null;
+				try {
+					for(int i=0; i<projects.length; i++) {
 						
-						count++;
-						if(count <= skippedNum || count > endNum) {
-							continue;
-						}
-						
-						if(!map.isEmpty()){
-							ReadEmpiricalTrial tmp = new ReadEmpiricalTrial();
-							tmp.setProject(projects[i]);
-							tmp.setBugID(String.valueOf(j));
+						for(int j=1; j<=bugNum[i]; j++) {
 							
-							ReadEmpiricalTrial t = map.get(tmp);
-							if(t==null){
-								System.err.println(projects[i]+"-"+j+" is missing.");
+							count++;
+							if(count <= skippedNum || count > endNum) {
 								continue;
 							}
 							
-							String deadEndType = t.getDeadEndType();
-							if(deadEndType==null || !(deadEndType.equals("control") || deadEndType.equals("data"))){
-								continue;
-							}
-							
+							if(!map.isEmpty()){
+								ReadEmpiricalTrial tmp = new ReadEmpiricalTrial();
+								tmp.setProject(projects[i]);
+								tmp.setBugID(String.valueOf(j));
+								
+								ReadEmpiricalTrial t = map.get(tmp);
+								if(t==null){
+									System.err.println(projects[i]+"-"+j+" is missing.");
+									continue;
+								}
+								
+								String deadEndType = t.getDeadEndType();
+								if(deadEndType==null || !(deadEndType.equals("control") || deadEndType.equals("data"))){
+									continue;
+								}
+								
 //							String exception = t.getException();
 //							if(exception==null || !exception.contains("over long")){
 //								continue;
 //							}
-						}
-						
-						System.out.println("***working on the " + j + "th bug of " + projects[i] + " project.");
-						
-						String buggyPath = prefix + projects[i] + File.separator + j + File.separator + "bug";
-						String fixPath = prefix + projects[i] + File.separator + j + File.separator + "fix";
-						
-						System.out.println("analyzing the " + j + "th bug in " + projects[i] + " project.");
-						
-						TrialGenerator generator = new TrialGenerator();
-						TrialGenerator0 generator0 = new TrialGenerator0();
-						
-						Defects4jProjectConfig d4jConfig = Defects4jProjectConfig.getD4JConfig(projects[i], j);
-						List<EmpiricalTrial> trials = generator0.generateTrials(buggyPath, fixPath, 
-								false, false, false, 3, false, true, d4jConfig, null);
-						
-						TrialRecorder recorder;
-						try {
-							recorder = new TrialRecorder();
-							recorder.export(trials, projects[i], j);
+							}
 							
-							for(EmpiricalTrial t: trials){
+							System.out.println("***working on the " + j + "th bug of " + projects[i] + " project.");
+							
+							String buggyPath = prefix + projects[i] + File.separator + j + File.separator + "bug";
+							String fixPath = prefix + projects[i] + File.separator + j + File.separator + "fix";
+							
+							System.out.println("analyzing the " + j + "th bug in " + projects[i] + " project.");
+							
+							TrialGenerator generator = new TrialGenerator();
+							TrialGenerator0 generator0 = new TrialGenerator0();
+							
+							Defects4jProjectConfig d4jConfig = Defects4jProjectConfig.getD4JConfig(projects[i], j);
+							List<EmpiricalTrial> trials = generator0.generateTrials(buggyPath, fixPath, 
+									false, false, false, 3, false, true, d4jConfig, null);
+							
+							TrialRecorder recorder;
+							try {
+								recorder = new TrialRecorder();
+								defects4jFile = recorder.export(trials, projects[i], j);
 								
-								if(!t.getDeadEndRecordList().isEmpty()){
-									Repository.clearCache();
-									DeadEndRecord record = t.getDeadEndRecordList().get(0);
+								for(EmpiricalTrial t: trials){
+									
+									if(!t.getDeadEndRecordList().isEmpty()){
+										Repository.clearCache();
+										DeadEndRecord record = t.getDeadEndRecordList().get(0);
 //									DED datas = new TrainingDataTransfer().transfer(record, t.getBuggyTrace());
-									DED datas = record.getTransformedData(t.getBuggyTrace());
-									setTestCase(datas, t.getTestcase());						
-									try {
+										DED datas = record.getTransformedData(t.getBuggyTrace());
+										setTestCase(datas, t.getTestcase());						
+										try {
 //										new DeadEndReporter().export(datas.getAllData(), projects[i], Integer.valueOf(j));
-										
-										if(!t.getRootCauseFinder().getCausalityGraph().getRoots().isEmpty()){
-											new DeadEndCSVWriter("_d4j", null).export(datas.getAllData(), projects[i], String.valueOf(j));											
+											
+											if(!t.getRootCauseFinder().getCausalityGraph().getRoots().isEmpty()){
+												new DeadEndCSVWriter("_d4j", null).export(datas.getAllData(), projects[i], String.valueOf(j));											
+											}
+										} catch (NumberFormatException | IOException e) {
+											e.printStackTrace();
 										}
-									} catch (NumberFormatException | IOException e) {
-										e.printStackTrace();
 									}
 								}
+							} catch (Exception e) {
+								e.printStackTrace();
 							}
-						} catch (Exception e) {
-							e.printStackTrace();
 						}
-						
 					}
+				} finally {
+					String oldDefects4jFile = Activator.getDefault().getPreferenceStore()
+							.getString(TregressionPreference.DEFECTS4J_FILE);
 					
+					if (defects4jFile != null && defects4jFile.exists() && oldDefects4jFile != null
+							&& new File(oldDefects4jFile).exists()) {
+						Map<String, List<String>> keys = new HashMap<String, List<String>>();
+						keys.put("data", Arrays.asList("project", "bug_ID"));
+						ExperimentRegressionTest.reportChange("defects4j_regressiontest.xlsx", oldDefects4jFile, defects4jFile.getAbsolutePath(), 
+									Arrays.asList(new TextComparationRule(null)), keys);
+					}
+				}
 					
 //					System.out.println("all the trials");
 //					for(int j=0; j<trials.size(); j++) {
 //						System.out.println("Trial " + (j+1));
 //						System.out.println(trials.get(j));
 //					}
-				}
-				
 				
 				return Status.OK_STATUS;
 			}
