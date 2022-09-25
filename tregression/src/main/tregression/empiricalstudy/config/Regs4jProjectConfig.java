ff2b0f5ad6f20b753b1b017b4de733e3e63053b1
bchenghi <bohchenghin@gmail.com>
2022-09-25 23:55:24 +0800

    Add new Regs4jProjectConfig

--- tregression/src/main/tregression/empiricalstudy/config/Regs4jProjectConfig.java
+++ tregression/src/main/tregression/empiricalstudy/config/Regs4jProjectConfig.java
@@ -1,20 +1,18 @@
 package tregression.empiricalstudy.config;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import core.MysqlManager;
-import model.Regression;
 import tregression.constants.Dataset;
 import tregression.empiricalstudy.TestCase;
 import tregression.handler.paths.PathConfigurationFactory;
 import tregression.handler.paths.Regs4jPathConfiguration;
-import com.google.protobuf.compiler.PluginProtos ;
-import com.mysql.cj.jdbc.Driver;
-import org.eclipse.jdt.internal.jarinjarloader.*;
 
 public class Regs4jProjectConfig extends MavenProjectConfig {
+	private final static String TEST_FILE_NAME = "tests.txt";
 	private Regs4jProjectConfig(String srcTestFolder, String srcSourceFolder, String bytecodeTestFolder,
 			String bytecodeSourceFolder, String buildFolder, String projectName, String bugID) {
 		super(srcTestFolder, srcSourceFolder, bytecodeTestFolder, bytecodeSourceFolder, buildFolder, projectName, bugID);
@@ -34,16 +32,17 @@ public class Regs4jProjectConfig extends MavenProjectConfig {
 	public List<TestCase> retrieveFailingTestCase(String buggyVersionPath) throws IOException {
 		Regs4jPathConfiguration regs4jPathConfiguration = (Regs4jPathConfiguration) PathConfigurationFactory.createPathConfiguration(Dataset.REGS4J);
 		String projectName = regs4jPathConfiguration.getProjectName(buggyVersionPath);
-		// Use MysqlManager from regs4j.jar to get regressions
-	    List<Regression> regressionList = MysqlManager.selectRegressions("select bfc,buggy,bic,work,testcase from regressions where project_full_name='" + projectName + "'");
-		String bugId = regs4jPathConfiguration.getBugId(buggyVersionPath);
-		Regression regression = regressionList.get(Integer.parseInt(bugId) - 1);
-		String testCaseStr = regression.getTestCase();
-//		result.add(new TestCase("com.univocity.parsers.issues.github.Github_420", "detectedFormatTest"));
-//		result.add(new TestCase("com.univocity.parsers.annotations.AnnotationHelperTest", "shouldCreateAnnotationHelper"));
+	    String bugId = regs4jPathConfiguration.getBugId(buggyVersionPath);
+	    String pathWithTestFile = regs4jPathConfiguration.getBugPath(projectName, bugId);
+	    byte[] encoded = Files.readAllBytes(Paths.get(pathWithTestFile, TEST_FILE_NAME));
+	    String testCaseStr = new String(encoded);
 		List<TestCase> result = new ArrayList<>();
 		String[] testClassAndName = testCaseStr.split("#");
+		String testMethod = testClassAndName[1].replaceAll("\r", "");
+		testMethod = testMethod.replaceAll("\n", "");
 		result.add(new TestCase(testClassAndName[0], testClassAndName[1]));
+//		result.add(new TestCase("com.univocity.parsers.issues.github.Github_420", "detectedFormatTest"));
+//		result.add(new TestCase("com.univocity.parsers.annotations.AnnotationHelperTest", "shouldCreateAnnotationHelper"));
 		return result;
 	}
 	
