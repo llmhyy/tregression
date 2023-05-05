aa11d953ab6fdb112087ea2fcb5483e85b00b65f
bchenghi <bohchenghin@gmail.com>
2023-05-05 18:47:50 +0800

    Update execution traces

--- tregression/test-dir/test/iodetection/IODetectorIT2.java
+++ tregression/test-dir/test/iodetection/IODetectorIT2.java
@@ -1,10 +1,15 @@
 package iodetection;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -12,6 +17,12 @@ import org.junit.jupiter.api.Test;
 
 import iodetection.IODetector.IOModel;
 
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 
 import microbat.instrumentation.output.RunningInfo;
@@ -19,6 +30,8 @@ import microbat.model.BreakPoint;
 import microbat.model.trace.Trace;
 import microbat.model.trace.TraceNode;
 import microbat.model.value.VarValue;
+import microbat.util.JavaUtil;
+import microbat.util.MinimumASTNodeFinder;
 import tregression.empiricalstudy.RootCauseFinder;
 import tregression.handler.PlayRegressionLocalizationHandler;
 import tregression.model.PairList;
@@ -30,15 +43,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 // TODO: Currently we are using traces from math_70 data set mutations. If the instrumentator & data set is updated, it may be impossible to reproduce the trace files with the exact same mutations (all tests will require updates).
 // Thus, the projects used to create the traces should be stored in the test-files directory. However, math_70 is very large.
 // We should create a small project that allows us to regenerate the traces used in the tests (and modify the tests to use them instead).
-class IODetectorIT {
+class IODetectorIT2 {
 
 	private static final Path traceDir = Paths.get("test-dir", "files", "iodetection");
 
-	private static final String SAMPLE_TEST_DIR = "src/main/test";
-
-	private IODetector constructIODetector(Trace buggyTrace, Trace workingTrace) {
-		return new IODetector(buggyTrace, workingTrace, SAMPLE_TEST_DIR);
+	private IODetector constructIODetector(Trace buggyTrace, Trace workingTrace, PairList pairList) {
+		return new IODetector2(buggyTrace, workingTrace, TEST_DIR, pairList);
 	}
+	
+	private static final String BUG_DIR_FORMAT = "E:\\david\\Mutation_Dataset\\math_70\\%d\\bug";
+	private static final String FIX_DIR = "E:\\david\\Mutation_Dataset\\math_70\\fix";
+	private static final String SRC_DIR = "src\\main\\java";
+	private static final String TEST_DIR = "src\\test\\java";
+	
+	private Map<String, CompilationUnit> pathToCompilationUnitMap;
+	
+	@BeforeEach
+	void setUp() {
+		 pathToCompilationUnitMap = new HashMap<>();
+	}
+	
 
 	// math_70 bug ID 1
 	@Test
@@ -49,7 +73,7 @@ class IODetectorIT {
 		Trace workingTrace = RunningInfo
 				.readFromFile(traceDir.resolve("working-SingleAssertionAndAllInputsInTest-trace.exec").toFile())
 				.getMainTrace();
-		IODetector detector = constructIODetector(buggyTrace, workingTrace);
+		IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 1));
 		detector.detect();
 		List<VarValue> expectedInputs = new ArrayList<>();
 		List<VarValue> expectedOutputs = new ArrayList<>();
@@ -77,22 +101,19 @@ class IODetectorIT {
 	void detect_MultipleAssertionsAndCrashFailure_ObtainsIOCorrectly() {
 
 	}
-
-	// To view the traces in the test case in the Eclipse application. Only works on
-	// Server 250.
-	void setRootCauseFinder(Trace buggyTrace, Trace workingTrace) {
+	
+	private PairList createPairList(Trace buggyTrace, Trace workingTrace, int bugId) {
 		// 4 arguments: relative src folder path, relative test folder path, working
 		// project root, buggy project root
 		// E.g. "src\\main\\java", "src\\test\\java",
 		// "E:\\david\\Mutation_Dataset\\math_70\\fix",
 		// "E:\\david\\Mutation_Dataset\\math_70\\1\\bug"
-		DiffMatcher diffMatcher = new DiffMatcher("", "", "", "");
+		String bugPath = String.format(BUG_DIR_FORMAT, bugId);
+		DiffMatcher diffMatcher = new DiffMatcher(SRC_DIR, TEST_DIR, FIX_DIR, bugPath);
 		diffMatcher.matchCode();
 		ControlPathBasedTraceMatcher traceMatcher = new ControlPathBasedTraceMatcher();
 		PairList pairList = traceMatcher.matchTraceNodePair(buggyTrace, workingTrace, diffMatcher);
-		RootCauseFinder rootcauseFinder = new RootCauseFinder();
-		rootcauseFinder.setRootCauseBasedOnDefects4J(pairList, diffMatcher, buggyTrace, workingTrace);
-		PlayRegressionLocalizationHandler.finder = rootcauseFinder;
+		return pairList;
 		// Run the left-most orange "Play the process of Regression Localization" button
 		// in eclipse application
 	}
@@ -108,18 +129,18 @@ class IODetectorIT {
 			Trace workingTrace = RunningInfo
 					.readFromFile(traceDir.resolve("working-SingleAssertionAndAllInputsInTest-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 1);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 1);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 1));
 			TraceNode outputNode = buggyTrace.getLatestNode();
 			outputNode.getReadVariables().get(0);
 			VarValue output = outputNode.getReadVariables().get(0);
 			List<VarValue> inputs = detector.detectInputVarValsFromOutput(outputNode, output);
 			Set<VarValue> expectedInputs = new HashSet<>();
-			for (VarValue input : buggyTrace.getTraceNode(2).getWrittenVariables()) {
+			for (VarValue input : buggyTrace.getTraceNode(1).getWrittenVariables()) {
 				expectedInputs.add(input);
 			}
-			expectedInputs.add(buggyTrace.getTraceNode(2).getReadVariables().get(0));
+			expectedInputs.add(buggyTrace.getTraceNode(1).getReadVariables().get(0));
 			assertEquals(expectedInputs, new HashSet<>(inputs));
 		}
 
@@ -132,16 +153,21 @@ class IODetectorIT {
 			Trace workingTrace = RunningInfo.readFromFile(
 					traceDir.resolve("working-MultipleAssertionsMultiLineAssertionWithEpsilon-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
-			TraceNode outputNode = buggyTrace.getTraceNode(1538);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 2);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 2);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 2));
+			TraceNode outputNode = buggyTrace.getTraceNode(1537);
 			outputNode.getReadVariables().get(0);
 			VarValue output = outputNode.getReadVariables().get(0);
 			List<VarValue> inputs = detector.detectInputVarValsFromOutput(outputNode, output);
 			Set<VarValue> expectedInputs = new HashSet<>();
 			// x at line 86, Math.cosh, sinh, tanh, log, ceil
 //			assertEquals(expectedInputs, new HashSet<>(inputs));
+			// Current inputs:
+			// CBRT, COSH, EXPM1, TANH, ABS, this, x, conditional result of loop, SQRT, SINH
+			// Does not have log, ceil (both used "postCompose", e.g. log.postCompose, while others used "of"), and has additional cbrt.
+			// Sometimes, the VarValues' children can form links as well.
+			// After fixing bug: sqrt, log, tanh, sinh, abs, ceil, expm1, cosh, cbrt
 		}
 
 		// math_70 bug ID 3
@@ -151,15 +177,20 @@ class IODetectorIT {
 					.getMainTrace();
 			Trace workingTrace = RunningInfo.readFromFile(traceDir.resolve("working-math_70-3-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 3);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 3);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 3));
 			TraceNode outputNode = buggyTrace.getLatestNode();
 			outputNode.getReadVariables().get(0);
 			VarValue output = outputNode.getReadVariables().get(0);
 			List<VarValue> inputs = detector.detectInputVarValsFromOutput(outputNode, output);
 			Set<VarValue> expectedInputs = new HashSet<>();
 //			assertEquals(expectedInputs, new HashSet<>(inputs));
+			// expected ComposableFunction.COS, 3, x
+			// After modifying to use getInvocationMethodOrControlDominator:
+			// We have COS, 0.1/x at line 357, 3/a, SIN, this at line 23, conditional result in for loop, 5/scaleFactor in line 398
+			// - Why did we get sin? Anonymous class data domination is not accurate. It will point to the previous same anonymous class initialization 
+			// node 45, this$0 -> node 38 (another anonymous ComposableFunction init)
 		}
 
 		// math_70 bug ID 5
@@ -169,25 +200,23 @@ class IODetectorIT {
 					.getMainTrace();
 			Trace workingTrace = RunningInfo.readFromFile(traceDir.resolve("working-ArrayInput-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 5);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 5);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 5));
 			TraceNode outputNode = buggyTrace.getLatestNode();
 			outputNode.getReadVariables().get(0);
 			VarValue output = outputNode.getReadVariables().get(0);
 			List<VarValue> inputs = detector.detectInputVarValsFromOutput(outputNode, output);
 			Set<VarValue> expectedInputs = new HashSet<>();
 //			assertEquals(expectedInputs, new HashSet<>(inputs));
-			// 1. drk should not be an input (correct). There could be initialization issues
-			// in constructor.
-			// 2. Values used such as 0.2, 0.2, 0.5 in initializing drk should be inputs
-			// 3. a, b, c in calling decode should be inputs as well.
-			// Based on current limitations, the inputs should be a, b, c, drk. (drk is
-			// included since, drk has no data dependency, due to no "read" value from
-			// constructor call)
-			// Issue:
-			// 1.Unsure why when obtaining data dominator, we do not get to drk. (order 63, 64)
-			// 2. Why it adds the output into the input
+			// Expected .2, .2, .5, a, b, c
+			// Current obtained inputs:
+			// We now have .2, .2, .5, a, b, c, drk, Double array.
+			// Questions:
+			// 1. Why do we have Double array, but not String array?
+			// Ans: String array is passed into Arrays.asList, which is excluded from instrumentation.
+			// 2. Why do we have drk & its original input? (Should only be original input)
+			// Ans: When initialization for drk is done, it is only written to drk. The constructor return value is not read.
 		}
 	}
 
@@ -202,11 +231,11 @@ class IODetectorIT {
 			Trace workingTrace = RunningInfo
 					.readFromFile(traceDir.resolve("working-SingleAssertionAndAllInputsInTest-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 1);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 1);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 1));
 			IOModel output = detector.detectOutput();
-			assertEquals(4, output.getNode().getOrder());
+			assertEquals(3, output.getNode().getOrder());
 			TraceNode outputNode = buggyTrace.getLatestNode();
 			outputNode.getReadVariables().get(0);
 			VarValue expectedVarVal = outputNode.getReadVariables().get(0);
@@ -222,12 +251,12 @@ class IODetectorIT {
 			Trace workingTrace = RunningInfo.readFromFile(
 					traceDir.resolve("working-MultipleAssertionsMultiLineAssertionWithEpsilon-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 2);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 2);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 2));
 			IOModel output = detector.detectOutput();
-			assertEquals(1538, output.getNode().getOrder());
-			TraceNode expectedOutputNode = buggyTrace.getTraceNode(1538);
+			assertEquals(1537, output.getNode().getOrder());
+			TraceNode expectedOutputNode = buggyTrace.getTraceNode(1537);
 			expectedOutputNode.getReadVariables().get(0);
 			VarValue expectedVarVal = expectedOutputNode.getReadVariables().get(0);
 			assertEquals(expectedVarVal, output.getVarVal());
@@ -240,9 +269,9 @@ class IODetectorIT {
 					.getMainTrace();
 			Trace workingTrace = RunningInfo.readFromFile(traceDir.resolve("working-math_70-3-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 3);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 3);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 3));
 			IOModel output = detector.detectOutput();
 			TraceNode expectedOutputNode = buggyTrace.getLatestNode();
 			expectedOutputNode.getReadVariables().get(0);
@@ -258,9 +287,9 @@ class IODetectorIT {
 					.getMainTrace();
 			Trace workingTrace = RunningInfo.readFromFile(traceDir.resolve("working-ArrayInput-trace.exec").toFile())
 					.getMainTrace();
-			setJavaPathToBreakPoints(buggyTrace.getExecutionList());
-			setJavaPathToBreakPoints(workingTrace.getExecutionList());
-			IODetector detector = constructIODetector(buggyTrace, workingTrace);
+			appendMissingInfoToExecutionList(buggyTrace.getExecutionList(), false, 5);
+			appendMissingInfoToExecutionList(workingTrace.getExecutionList(), true, 5);
+			IODetector detector = constructIODetector(buggyTrace, workingTrace, createPairList(buggyTrace, workingTrace, 5));
 			IOModel output = detector.detectOutput();
 			TraceNode expectedOutputNode = buggyTrace.getLatestNode();
 			expectedOutputNode.getReadVariables().get(0);
@@ -270,26 +299,92 @@ class IODetectorIT {
 		}
 	}
 
-	private void setJavaPathToBreakPoints(List<TraceNode> executionList) {
-		Predicate<TraceNode> testNodeFilter = new Predicate<TraceNode>() {
-			@Override
-			public boolean test(TraceNode node) {
-				BreakPoint breakPoint = node.getBreakPoint();
-				return breakPoint.getClassCanonicalName().endsWith("Test");
-			}
-		};
-		setJavaPathToBreakPoints(executionList, testNodeFilter);
+	private void appendMissingInfoToExecutionList(List<TraceNode> executionList, boolean isWorking, int id) {
+		setJavaPathToBreakPoints(executionList, isWorking, id);
+		setMissingReadVariables(executionList);
 	}
-
-	private void setJavaPathToBreakPoints(List<TraceNode> executionList, Predicate<TraceNode> testNodeFilter) {
+	
+	private void setJavaPathToBreakPoints(List<TraceNode> executionList,  boolean isWorking, int id) {
 		for (int i = 0; i < executionList.size(); i++) {
 			TraceNode node = executionList.get(i);
 			BreakPoint breakPoint = node.getBreakPoint();
-			if (testNodeFilter.test(node)) {
-				breakPoint.setFullJavaFilePath(SAMPLE_TEST_DIR);
+			String path = "";
+			if (isWorking) {
+				path = FIX_DIR;
 			} else {
-				breakPoint.setFullJavaFilePath("");
+				path = String.format(BUG_DIR_FORMAT, id);
+			}
+			String className = breakPoint.getClassCanonicalName();
+			if (className.contains("$")) {
+				className = className.substring(0, className.indexOf("$"));
+			}
+			String classPath = className.replace(".", File.separator) + ".java";
+			String testPath = String.join(File.separator, path, TEST_DIR, classPath);
+			if (new File(testPath).exists()) {
+				breakPoint.setFullJavaFilePath(testPath);
+			} else {
+				breakPoint.setFullJavaFilePath(String.join(File.separator, path, SRC_DIR, classPath));
 			}
 		}
 	}
+	
+	private void setMissingReadVariables(List<TraceNode> executionList) {
+		for(TraceNode node: executionList){
+			if(!node.getInvocationChildren().isEmpty() && 
+					node.getReadVariables().isEmpty()) {
+				//check AST completeness
+				CompilationUnit cu;
+				try {
+					cu = getCompilationUnit(Paths.get(node.getBreakPoint().getFullJavaFilePath()));
+				} catch (IOException e) {
+					e.printStackTrace();
+					continue;
+				}
+				MinimumASTNodeFinder finder = new MinimumASTNodeFinder(
+						node.getLineNumber(), cu);
+				cu.accept(finder);
+				ASTNode astNode = finder.getMinimumNode();
+				
+				if(astNode!=null) {
+					int start = cu.getLineNumber(astNode.getStartPosition());
+					int end = cu.getLineNumber(astNode.getStartPosition()+astNode.getLength());
+					
+					TraceNode stepOverPrev = node.getStepOverPrevious();
+					while(stepOverPrev!=null && 
+							start<=stepOverPrev.getLineNumber() &&
+							stepOverPrev.getLineNumber()<=end) {
+						List<VarValue> readVars = stepOverPrev.getReadVariables();
+						for(VarValue readVar: readVars) {
+							if(!node.getReadVariables().contains(readVar)) {
+								node.getReadVariables().add(readVar);
+							}
+						}
+						stepOverPrev = stepOverPrev.getStepOverPrevious();
+					}
+				}
+				
+			}
+		}
+	}
+	
+	private CompilationUnit getCompilationUnit(Path path) throws IOException {
+		String pathStr = path.toString();
+		if (pathToCompilationUnitMap.containsKey(pathStr)) return pathToCompilationUnitMap.get(pathStr);
+		String fileContent = Files.readString(path);
+		CompilationUnit cu = parseCompilationUnit(fileContent);
+		pathToCompilationUnitMap.put(pathStr, cu);
+		return cu;
+		
+	}
+    private CompilationUnit parseCompilationUnit(String fileContent) {
+        ASTParser parser = ASTParser.newParser(AST.getJLSLatest()); // handles JDK 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+        parser.setSource(fileContent.toCharArray());
+        parser.setResolveBindings(true);
+        // In order to parse 1.6 code, some compiler options need to be set to 1.6
+        Map<String, String> options = JavaCore.getOptions();
+        JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
+        parser.setCompilerOptions(options);
+
+        return (CompilationUnit) parser.createAST(null);
+    }
 }
