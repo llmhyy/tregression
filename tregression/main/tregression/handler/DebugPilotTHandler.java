18dd823497b1baedc9f51f68aabf1f358ab69874
david2 <david2@172.26.191.20>
2023-07-19 00:39:09 +0800

    Update UI for DebugPilot

---
+++ tregression/main/tregression/handler/DebugPilotTHandler.java
@@ -0,0 +1,36 @@
+package tregression.handler;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+import microbat.handler.DebugPilotHandler;
+import microbat.views.PathView;
+import tregression.views.BuggyTraceView;
+import tregression.views.CorrectTraceView;
+
+public class DebugPilotTHandler extends DebugPilotHandler {
+	
+	protected CorrectTraceView correctView;
+	
+	@Override
+	protected void setup() {
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					buggyView = (BuggyTraceView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(BuggyTraceView.ID);
+					correctView = (CorrectTraceView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(CorrectTraceView.ID);
+					pathView = (PathView) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(PathView.ID);
+					pathView.setBuggyView(buggyView);
+					
+				} catch (PartInitException e) {
+					buggyView = null;
+					correctView = null;
+					pathView = null;
+					System.out.println("Fail to get the view");
+				}
+			}
+		});
+	}
+}
